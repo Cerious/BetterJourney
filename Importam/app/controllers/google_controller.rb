d3ffc51ef998eddd715fcbde7068c26eb34d2bae
Nick Ellis <217nickellis@gmail.com>
2018-10-15 19:47:23 -0400

    working on API call code

--- Importam/app/controllers/google_controller.rb
+++ Importam/app/controllers/google_controller.rb
@@ -2,6 +2,12 @@ class GoogleController < ApplicationController
   def import
   end
 
+  def authenticate
+  end
+
+  def authenticated
+  end
+
   def help
   end
 end
--- Importam/config/routes.rb
+++ Importam/config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'google/help'
   get 'static_pages/help'
   get 'static_pages/google'
+  get 'google/authenticated'
 
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
