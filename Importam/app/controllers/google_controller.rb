f28fd81078455c520071d1b724c2b792fcbe1f31
Nick Ellis <217nickellis@gmail.com>
2018-10-16 05:29:52 -0400

    finnaly got some ode for the authentication

--- Importam/app/controllers/google_controller.rb
+++ Importam/app/controllers/google_controller.rb
@@ -2,12 +2,17 @@ class GoogleController < ApplicationController
   def import
   end
 
-  def authenticate
-  end
-
   def authenticated
   end
 
   def help
+  end
+
+  def authenticate
+    client_secrets = Google::APIClient::ClientSecrets.load
+
+    # Request authorization
+    redirect user_credentials.authorization_uri.to_s, 303
+
   end
 end
