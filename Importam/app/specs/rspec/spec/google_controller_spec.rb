5a76eb33de4bdf8b1687875ac3ae35b41dcd83e8
Nick Ellis <217nickellis@gmail.com>
2018-11-03 18:00:17 -0400

    started testing actual app functionality.

--- Importam/app/specs/rspec/spec/google_controller_spec.rb
+++ Importam/app/specs/rspec/spec/google_controller_spec.rb
@@ -2,10 +2,11 @@ describe GoogleController do
     before do
         response = HTTParty.post('https://accounts.google.com/o/oauth2/token',  body: { client_id: ENV["GOOGLEID"], client_secret: ENV["GOOGLESECRET"], code: params['code'], grant_type: 'authorization_code', redirect_uri: 'http://localhost:3000/google/oauth2callback', scope: params['scope'] }, headers: { 'Accept' => 'application/json' } )
         access_token = response["access_token"]
+    end
     
     describe "Token response" do
         it "is not empty" do
             expect(access_token).not_to be nil
+        end
     end
-
 end
