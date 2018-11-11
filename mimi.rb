4e7eccbbcb0f18e2e28934b68c815133b77f9776
Nick Ellis <217nickellis@gmail.com>
2018-11-11 13:48:32 -0500

    fixed body

--- mimi.rb
+++ mimi.rb
@@ -6,28 +6,25 @@ class SubberTool
     ##Along with any request, you'll need to pass two parameters. The first is your 
     ##Username (the email you use to sign in), which identifies the account you're 
     ##currently using, and the second is a valid API key.
-BASE_URL = 'https://madmimi.com:443/api/v3/subscriberLists?name=jason'
-CREDS = 'curl -v -H "Accept: application/json" -X GET --data "api_key=nr;Uq6qKY8AhjTm&username=justin@mybetterjourney.com https://api.madmimi.com/promotions'
-def add_sub
-    name = 'carl'
-    list_info = {
+    BASE_URL = 'https://madmimi.com:443'
+    CREDS = 'curl -v -H "Accept: application/json" -X GET --data "api_key=nr;Uq6qKY8AhjTm&username=justin@mybetterjourney.com https://api.madmimi.com/promotions'
+    def add_sub
 
-    }
-    person = {
-        "email": "carl2cool@gmail.com",
-      "name": "carl bravo",
-      "firstName": "carl",
-      "lastName": "bravo",
-    }
-    HTTParty.post("#{BASE_URL}:443/api/v3/subscriberLists?name=#{name}", 
-    query{auth: CREDS}, 
-    headers: {
-        "Content-Type" => "application/json"
-        "Accept" => "application/json"
-      })
+        }
+        person = {
+            "email": "carl2cool@gmail.com",
+            "firstName": "carl",
+            "lastName": "bravo"
+        }
+        HTTParty.post("#{BASE_URL}/api/v3/subscribers", 
+        body: person, 
+        headers: {
+            "Content-Type": "application/json"
+            "Accept": "application/json"
+        })
 
-end
+    end
 
-def add_person
+    def add_person
 
 end
