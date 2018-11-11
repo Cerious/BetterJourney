37062650b7b967b5e5ec6af247877bffb39e6349
Nick Ellis <217nickellis@gmail.com>
2018-11-11 16:11:06 -0500

    figured out what a query string is.

--- mimi.rb
+++ mimi.rb
@@ -1,30 +1,34 @@
-##In this we will be using the mad mimi sub api to first create a person and then
-##add said person to the subscriber list.
-##use httparty and find out the structure for sending a request with it
-
+require 'httparty'
+require 'json'
 class SubberTool
-    ##Along with any request, you'll need to pass two parameters. The first is your 
-    ##Username (the email you use to sign in), which identifies the account you're 
-    ##currently using, and the second is a valid API key.
     BASE_URL = 'https://madmimi.com:443'
-    CREDS = 'curl -v -H "Accept: application/json" -X GET --data "api_key=nr;Uq6qKY8AhjTm&username=justin@mybetterjourney.com https://api.madmimi.com/promotions'
+    CREDS = 'https://api.madmimi.com/promotions?username=justin@mybetterjourney.com&api_key=nr;Uq6qKY8AhjTm'
+    
     def add_sub
-
-        }
+        HTTParty.get(CREDS)
         person = {
             "email": "carl2cool@gmail.com",
             "firstName": "carl",
             "lastName": "bravo"
-        }
+    }.to_json
         HTTParty.post("#{BASE_URL}/api/v3/subscribers", 
         body: person, 
         headers: {
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
             "Accept": "application/json"
         })
 
     end
 
-    def add_person
+    def get_mock_list(listId=6151744)
+        HTTParty.get("#{BASE_URL}/api/v3/subscriberLists/#{listId}/subscribers",
+        headers: {
+            "Accept": "application/json"
+        } )
+        
+    end 
 
 end
+
+carl = SubberTool.new
+puts carl.add_sub
