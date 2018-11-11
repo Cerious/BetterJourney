62e28a4d0ee2901a77223c8d8ae37a98d0c983db
Nick Ellis <217nickellis@gmail.com>
2018-11-10 23:38:26 -0500

    working on file to add people to subscriber list

---
+++ mimi.rb
@@ -0,0 +1,11 @@
+##In this we will be using the mad mimi sub api to first create a person and then
+##add said person to the subscriber list.
+##use httparty and find out the structure for sending a request with it
+
+def add_sub
+
+end
+
+def add_person
+    
+end
